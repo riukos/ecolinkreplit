62d562039ba4d00048862edcff1699af7647c207
FelipeAndrade <riiuuku@gmail.com>
2023-10-18 11:28:00 -0300

    a

--- .flaskenv
+++
@@ -1,3 +0,0 @@
-FLASK_APP = run.py
-FLASK_DEBUG = true
-FLASK_ENV = development
--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'felipe123'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or  'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQSQLALCHEMY_TRACK_MODIFICATIONS=False
